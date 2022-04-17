6718e1ff9aab29fab095a08e5b7e798d300ed5fd
jhenriqueb12 <henrique.barros.hb@outlook.com>
2022-04-17 17:18:14 -0300

    Fundamentos de JavaScript

---
+++ JavaScript/2-Estrutura-lógica/Operadores de comparação.js
@@ -0,0 +1,34 @@
+const value1 = 1;
+const value2 = 2;
+
+// Maior que
+var resultMaior = value1 > value2;
+console.log(resultMaior);
+
+// Maior ou igual
+var resultMaiorIgual = value1 >= value2;
+console.log(resultMaiorIgual);
+
+// Menor que
+var resultMenor = value1 < value2;
+console.log(resultMenor);
+
+// Menor ou igual
+var resultMenorIgual = value1 <= value2;
+console.log(resultMenorIgual);
+
+// Igualdade
+var resultIgual = value1 == value2;
+console.log(resultIgual);
+
+// Igualdade irrestrita
+var resultRestrita = value1 === value2;
+console.log(resultRestrita);
+
+// Diferença
+var resultDiferenca = value1 != value2;
+console.log(resultDiferenca);
+
+// Menor que
+var resultDiferencaRestrita = value1 !== value2;
+console.log(esultDiferencaRestrita);
